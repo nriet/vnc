97fc062d50658a34c27e909fbfea0ea9efadcc76
axiu <22583574@qq.com>
2024-08-22 01:00:30 +0800

    Update install_idea.sh

--- src/ubuntu/install/idea/install_idea.sh
+++ src/ubuntu/install/idea/install_idea.sh
@@ -2,20 +2,18 @@
 
 IDEA_VER_DATE="2023.3.3-aarch64"
 
-cd /tmp
+cd /opt
 wget -q -O idea.tar.gz "https://download.jetbrains.com.cn/idea/ideaIU-${IDEA_VER_DATE}.tar.gz"
-
-tar -xzf idea.tar.gz -C /opt
-
-mv /opt/idea-IU-233.11799.241 /opt/idea
 
 idea_ICON="/opt/idea/plugins/$(ls /opt/idea/plugins/ | grep -m 1 org.idea.platform_)/idea.png"
 sed -i "s#idea.png#${idea_ICON}#" $INST_SCRIPTS/idea/idea.desktop
 cp $INST_SCRIPTS/idea/idea.desktop $HOME/Desktop/
 cp $INST_SCRIPTS/idea/idea.desktop /usr/share/applications/
 
-#chmod +x $INST_SCRIPTS/idea/jetbra/scripts/install.sh
+mv $INST_SCRIPTS/idea/jetbra /opt/jetbra
 
-#./$INST_SCRIPTS/idea/jetbra/scripts/install.sh
+chmod +x /opt/jetbra/scripts/install.sh
+
+./opt/jetbra/scripts/install.sh
 
 rm -rf /tmp/idea.tar.gz
