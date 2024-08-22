1f7a94473feb270652801bd5c5cb2eec605f6ae7
22583574@qq.com <22583574@qq.com>
2024-08-22 13:22:52 +0800

    nriet

--- src/ubuntu/install/idea/install_idea.sh
+++ src/ubuntu/install/idea/install_idea.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#修改用户名称
+usermod -l nriet kasm-user
+sed -i 's/kasm-user/nriet/g' /etc/passwd
+sed -i 's/kasm-user/nriet/g' /etc/shadow
+mv /home/kasm-user /home/nriet
+
 IDEA_VER_DATE="2023.3.3"
 
 ARCH=$(arch | sed 's/aarch64/arm64/g' | sed 's/x86_64/amd64/g')
@@ -11,19 +17,15 @@ fi
 cd /tmp
 wget -q -O idea.tar.gz "https://download.jetbrains.com.cn/idea/ideaIU-${IDEA_VER_DATE}.tar.gz"
 
-tar -xzf idea.tar.gz -C /opt
+tar -xzf idea.tar.gz -C /tmp
 
-mv /opt/idea-* /opt/idea
+mv /tmp/idea-* $HOME/idea
 
-idea_ICON="/opt/idea/plugins/$(ls /opt/idea/plugins/ | grep -m 1 org.idea.platform_)/idea.png"
-sed -i "s#idea.png#${idea_ICON}#" $INST_SCRIPTS/idea/idea.desktop
 cp $INST_SCRIPTS/idea/idea.desktop $HOME/Desktop/
 cp $INST_SCRIPTS/idea/idea.desktop /usr/share/applications/
 
-mv $INST_SCRIPTS/idea/jetbra /opt/jetbra
+mv $INST_SCRIPTS/idea/jetbra $HOME/jetbra
 
-chmod +x /opt/jetbra/scripts/*.sh
-
-./opt/jetbra/scripts/install.sh
+chmod +x $HOME/jetbra/scripts/*.sh
 
 rm -rf /tmp/idea.tar.gz
